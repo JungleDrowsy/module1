f8670d4014edf49a3511ab59a3c6c6cde523c082
JungleDrowsy <bradley.mills@snhu.edu>
2024-08-07 15:46:12 -0500

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors"); // Import the CORS package
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -19,17 +20,24 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-
-//register handebars patials (https://www.npmjs.com/package/hbs)
+// Register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extendxed: false }));
+app.use(express.urlencoded({ extended: false })); // Fixed typo 'extendxed'
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable CORS with the CORS package
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,10 +7,12 @@ const tripsController = require('../controllers/trips');
 // Define routes
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripList
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET Method routes tripsFindByCode
+    .get(tripsController.tripsFindByCode) // GET Method routes tripsFindByCode
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
